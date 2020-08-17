b3180dd02fc1768c39aea2d93ae33891dc1c2320
bbyji07 <68051582+bbyji07@users.noreply.github.com>
2020-08-18 00:25:44 +0530

    update groovy file

--- firstPipe.groovy
+++ firstPipe.groovy
@@ -1,18 +1,8 @@
-final mysharedlibrary1 = library('mysharedlibrary1')
-
-def execute(String zCmakeListsPath){
-  stage('some kind of stage 2') {
-    echo "Hello from stage 1 with " + zCmakeListsPath
-    echo "var attempt ${env.mySrcDir}"
-
-  }
-  stage('second stage'){
-    echo "and one from stage 2"
-    echo "param was " + zCmakeListsPath
-    echo "var attempt ${env.myBuildDir}"
-    //call function from global lib
-    mysharedlibrary1.deleteFile 'for 3rd party global library now'
-  }
+vars/log.groovy
+def info(message) {
+    echo "INFO: ${message}"
 }
 
-return this
+def warning(message) {
+    echo "WARNING: ${message}"
+}
